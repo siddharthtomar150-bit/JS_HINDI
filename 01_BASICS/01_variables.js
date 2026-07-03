6a2fe61b92fffd76e93d21eaeb5c059089375c2d
siddharthtomar150-bit <siddharthtomar150@gmail.com>
2026-07-03 08:40:35 +0000

    variables

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=1
+let accountEmail="siddharthtomar150@gmail.com"
+var accountPassword="148135"
+let accountCity="meerut"
+let accountState;
+
+
+/*
+prefer not to use var
+because  of issue in block scoppe and functional scope
+*/
+
+// accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
